dfd28422a57b84b9aff91840bfe0f677ecfdc484
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-10-18 18:54:26 -0300

    Comienzo de modelo de password

--- passwords/models.py
+++ passwords/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class PasswordInfo(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
+    user_or_email = models.CharField(max_length=100)
+    password = models.CharField(max_length=200)
+    additional_notes = models.TextField(blank=True)
+    type_of_password = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
