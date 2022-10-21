c6f0bd2806278102f84739b43af821aa8c457589
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-10-21 19:37:03 -0300

    Modificado layout para usar includes. Funcionalidades signin, signout, signup.

--- passwords/urls.py
+++ passwords/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import *
 
+
 # Viene de django y sirve para links 
-app_name = "passwords" 
+#app_name = "passwords" 
 
 urlpatterns = [
     path("", home, name="home"),
-    path("signin", signin, name="signin"),
-    path("signup", signup, name="signup")
-    #path("<int:post_id>", post_detail, name="post_detail")
+    path("signin/", signin, name="signin"),
+    path("signup/", signup, name="signup"),
+    path("signout/", signout, name="signout"),
+    path("passwords/", passwords, name="passwords")
 ]
--- passwords/views.py
+++ passwords/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from authuser.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -7,7 +11,42 @@ def home(request):
 def signin(request):
     if request.method == "GET":
         return render(request, 'signin.html')
+    else:
+        user = authenticate(
+            request, email=request.POST['email'], password=request.POST['password']
+        )
+        
+        if user is None:
+            return render(request, 'signin.html', {
+                'error': 'Email or password incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('passwords') 
+
+
+
+
 
 def signup(request):
     if request.method == "GET":
         return render(request, 'signup.html')
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create(email=request.POST['email'], password=request.POST['password1'])
+                login(request, user)
+                return redirect('passwords')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'error': 'Email already exists'
+                })
+
+@login_required            
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+@login_required 
+def passwords(request):
+    return render(request, 'passwords.html')
